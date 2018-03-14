35b22cca3e750d3178729fec7706230f6459c6d6
Bryan Cary <35739623+OsmosisJonesOverHeaven@users.noreply.github.com>
2018-03-14 09:58:36 -0500

    Add files via upload
    File contains properties of each item. Will be edited at item instantiation.

---
+++ PartProperties.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartProperties : MonoBehaviour {
+
+    public string partName;         //what kind of part is it?
+    public string fitment;      //ATX, NLX, MINI-ATX, if none in particular put "ALL"
+    public bool dead = false;
+
+
+
+    private void Start()
+    {
+        
+    }
+
+}
