678ddcffe6d53bbddea2a2e6ebd8c7870207349b
Bryan Cary <35739623+OsmosisJonesOverHeaven@users.noreply.github.com>
2018-03-14 09:31:48 -0500

    Updated PlayerControls.cs
    Crouch added. Movement now uses Time.deltaTime to make you move the same speed on any performance. Camera now has angular bounds. Added a reference to the rig object that "cam" is now under.

--- PlayerControls.cs
+++ PlayerControls.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour {
 
     GameObject cam;
-	
-	void Start () {
+    GameObject rig;
+    float camHeight;
+    float crouchHeight;
+
+    void Start () {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-        cam = this.transform.GetChild(0).gameObject;
+        rig = this.transform.GetChild(0).gameObject;
+        cam = rig.transform.GetChild(0).gameObject;
+        camHeight = cam.transform.position.y;
+        crouchHeight = cam.transform.position.y - 1;
 	}
+
+    //debug vars
+    //public float testCamRotX;
+    //public float tcr;
+    public float camPosy;
 
     //Camera X and Y controls
     float camX, camY;
@@ -18,30 +29,47 @@ public class PlayerControls : MonoBehaviour {
     float camSpeedY = 5.0f;
     //Player X and Z controls
     float plX, plZ;
-    float speed;
-    float speedDefault = 0.1f;
+    float speed = 4;
+    //float speedDefault = 0.1f;
 
-	void Update () {
+    float crouchSpeed = 5.5f;
+
+    void Update() {
+        //debug
+        //testCamRotX = Input.GetAxis("Mouse Y");
+        //tcr = cam.transform.localRotation.x;
+        //camPosy = cam.transform.localPosition.y;
+        //debug
+
         transform.Rotate(0, camY, 0);
-        plZ = Input.GetAxis("Vertical") * speed;
-        plX = Input.GetAxis("Horizontal") * speed;
-
-        cam.transform.Rotate(camX, 0, 0);
+        plZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        plX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         camY = Input.GetAxis("Mouse X") * camSpeedY;
         camX = -Input.GetAxis("Mouse Y") * camSpeedX;
 
-        if (Input.GetKey(KeyCode.LeftControl))
-        {
-            speed = speedDefault / 2;
+        if((cam.transform.localRotation.x < 0.6 && camX > 0) || (cam.transform.localRotation.x > -0.6 && camX < 0)){
+            cam.transform.Rotate(camX, 0, 0);
         }
-        else
-        {
-            speed = speedDefault;
-        }
+
+        
 	}
 
     private void FixedUpdate()
     {
-        transform.Translate(plX, 0, plZ); 
+        transform.Translate(plX, 0, plZ);
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            if(cam.transform.position.y > crouchHeight)
+            {
+                rig.transform.Translate(0, -crouchSpeed * Time.deltaTime, crouchSpeed/2*Time.deltaTime);
+            }
+        }
+        else
+        {
+            if (cam.transform.position.y < camHeight)
+            {
+                rig.transform.Translate(0, crouchSpeed * Time.deltaTime, -crouchSpeed / 2 * Time.deltaTime);
+            }
+        }
     }
 }
