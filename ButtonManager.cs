8869e153dab652ce16c5c14f31f2f619eddbf85a
Jonathan Roussel <35738556+jrous47@users.noreply.github.com>
2018-03-21 21:34:46 -0500

    Button Manager
    +Start Menu Button
    +Quit Menu Button

---
+++ ButtonManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+    public void BeginSimulation(string newSimulation)
+    {
+        SceneManager.LoadScene(newSimulation);
+    }
+    
+    public void LeaveSimulation()
+    {
+        Application.Quit();
+    }
+}
