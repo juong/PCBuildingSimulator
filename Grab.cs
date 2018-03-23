68badef42f1a860eb4e2a1ecd75be34553057012
Bryan Cary <35739623+OsmosisJonesOverHeaven@users.noreply.github.com>
2018-03-23 12:20:11 -0500

    Placement code added

--- Grab.cs
+++ Grab.cs
@@ -6,17 +6,24 @@ public class Grab : MonoBehaviour {
     public bool handFree = true;
     public GameObject item;
     public Transform guide;
+    public GameObject MB;
+    GameObject parts;
+
+    public float distance;
+    float maxdist;
+    
+    private GameObject placedItem; //only used if there we are pulling an item off
 
     void OnTriggerEnter(Collider col)
     {
-        if (col.gameObject.tag == "item")
+        if (col.gameObject.tag == "item" || col.gameObject.tag == "MB")
             if (!item)
                 item = col.gameObject;
     }
 
     void OnTriggerExit(Collider col)
     {
-        if (col.gameObject.tag == "item")
+        if (col.gameObject.tag == "item" || col.gameObject.tag == "MB")
         {
             if (handFree)
                 item = null;
@@ -24,33 +31,47 @@ public class Grab : MonoBehaviour {
     }
 
     void Start () {
-		
-	}
+        
+    }
 	
 	void Update () {
+        if (item && (item.name.StartsWith("mb") || item.name.StartsWith("hdd") || item.name.StartsWith("per") || item.name.StartsWith("psu")))
+            maxdist = 0.6f;
+        else
+            maxdist = 0.3f;
         if (Input.GetMouseButtonDown(0))
         {
             if (!handFree)
-                drop();
+                if (distance < maxdist && (!item.GetComponent<PartProperties>().isMB))
+                    place();
+                else
+                    drop();
             else
                 pickup();
-        }//mause If
+        }
 
-        if (!handFree && item)
+        if (!handFree && item) {
             item.transform.position = guide.position;
+            if(!item.GetComponent<PartProperties>().isMB)
+                distance = Vector3.Distance(item.transform.position, item.GetComponent<PartProperties>().target.transform.position);
+        }
     }
 
     private void pickup()
     {
         if (item)
         {
-            item.GetComponent<Collider>().enabled = false;
+            if (item.GetComponent<PartProperties>().placed)
+            {
+                placedItem = item;
+                item.GetComponent<PartProperties>().duplicate();
+                item = item.GetComponent<PartProperties>().dupe;
+            }
+            item.GetComponent<Collider>().isTrigger = true;
             //Set the object parent to the guide empty object.
             item.transform.SetParent(guide);
             //Set gravity to false while holding item
             item.GetComponent<Rigidbody>().useGravity = false;
-            //Apply the same rotation the main object has.
-            item.transform.localRotation = transform.rotation;
             //Re-position the ball on the guide object 
             item.transform.position = guide.position;
             handFree = false;
@@ -61,14 +82,66 @@ public class Grab : MonoBehaviour {
     {
         if (item)
         {
-            item.GetComponent<Collider>().enabled = true;
+            item.GetComponent<Collider>().isTrigger = false;
             //Set the Gravity to true again.
             item.GetComponent<Rigidbody>().useGravity = true;
             //Item is now no longer useful
-            item = null;
+            //item = null;
             //Unparent our ball
             guide.GetChild(0).parent = null;
             handFree = true;
         }
     }
+
+    private void place()
+    {
+        if (item && (item.name.StartsWith("mb") || MB))
+        {
+
+            if (item.name.StartsWith("ram") || item.name.StartsWith("video_card"))
+            {
+                sort(item.name);
+            }
+            item.GetComponent<PartProperties>().target.SetActive(true);
+            if (item.name.StartsWith("mb"))
+            {
+                MB = item.GetComponent<PartProperties>().target.transform.GetChild(0).gameObject;
+            }
+            Destroy(item);
+            item = null;
+            handFree = true;
+        }
+    }
+
+    private void sort(string type)
+    {
+        bool targetFound = false;
+        if (type.StartsWith("r"))
+        {
+            for(int i = 0; i < 4; i++)
+            {
+                if(!MB.transform.GetChild(1).GetChild(i + 4).gameObject.activeSelf && !targetFound)
+                {
+                    item.GetComponent<PartProperties>().target = MB.transform.GetChild(1).GetChild(i + 4).gameObject;
+                    targetFound = true;
+                }
+            }
+        }
+        if (type.StartsWith("v"))
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (!MB.transform.GetChild(1).GetChild(i + 8).gameObject.activeSelf && !targetFound)
+                {
+                    item.GetComponent<PartProperties>().target = MB.transform.GetChild(1).GetChild(i + 8).gameObject;
+                    targetFound = true;
+                }
+            }
+        }
+    }
+
+    /*TBA
+     * add ref to mb
+     * query when special parts are added
+     */
 }
--- PartProperties.cs
+++ PartProperties.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PartProperties : MonoBehaviour {
 
-    public string partName;         //what kind of part is it?
+    //informational set these on instantiation
+    public string partName;         //what kind of part is it? (cpu, cpuFan, vidCard, ram, periph, psu, or hdd)
     public string fitment;      //ATX, NLX, MINI-ATX, if none in particular put "ALL"
+    public GameObject target;
+    public GameObject dupe;
     public bool dead = false;
+    public string specs;
 
-
+    public bool placed = false;
+    public bool isMB = false;
+    //keeps track of which slots are taken
+    //only used if isMB is true
+    bool hasCPU = false;
+    bool hasCPUFan = false;
+    bool hasVidCard = false;
+    bool hasRAM = false;
+    bool hasHDD = false;
+    bool hasPSU = false;
+    bool hasPeriph = false;
+    int RAMCount = 0;
+    int VCCount = 0;
 
     private void Start()
     {
-        
+        if (this.tag == "MB")
+            isMB = true;
+    }
+
+    public void duplicate()
+    {
+        if (!isMB)
+        {
+            dupe = Instantiate(this.gameObject);
+            dupe.GetComponent<PartProperties>().placed = false;
+            //this.gameObject.SetActive(false);
+        }
     }
 
 }
