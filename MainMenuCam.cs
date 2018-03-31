17891764a542f07aa6f120072c2896658a1bbe96
Bryan Cary <35739623+OsmosisJonesOverHeaven@users.noreply.github.com>
2018-03-31 02:24:39 -0500

    Switching tools in menu, Main Menu cam

---
+++ MainMenuCam.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCam : MonoBehaviour {
+
+    GameObject guide;
+    public GameObject[] obj;
+    public bool isItem;
+
+    int complexity = 50; //has to be larger than size, or it makes weird shapes, higher number causes higher load
+    int size = 25;
+    GameObject par;
+
+    Vector3 rotationSpeed;
+
+    void Start () {
+        guide = GameObject.FindGameObjectWithTag("tool");
+        if (!isItem)
+        {
+            par = guide.transform.GetChild(0).gameObject;
+            guide.transform.Translate(0, -size, 0);
+            for (int i = 1; i < size * 2; i++)
+            {
+                //Instantiate(obj[Random.Range(0, obj.Length - 1)], guide.transform.position, Random.rotation);
+                for (int j = 0; j < complexity; j++)
+                {
+                    Instantiate(obj[Random.Range(0, obj.Length - 1)], par.transform.position, Random.rotation);
+                    guide.transform.Rotate(0, 360 / complexity + Random.Range(-360 / (complexity / 2), 360 / (complexity / 2)), 0);
+                }
+                guide.transform.Translate(0, size / (complexity / 2), 0);
+                if (guide.transform.position.y <= 0)
+                {
+                    par.transform.Translate(0, 0, 2 * size / (complexity / 2));
+                }
+                else
+                {
+                    par.transform.Translate(0, 0, 2 * -size / (complexity / 2));
+                }
+            }
+        }
+        else
+        {
+            rotationSpeed = new Vector3(Random.Range(0.01f, 1.0f), Random.Range(0.01f, 1.0f), Random.Range(0.01f, 1.0f));
+        }
+	}
+	
+	
+	void Update () {
+        if (isItem)
+        {
+            this.transform.RotateAround(guide.transform.position, guide.transform.position, 0.1f);
+            transform.Rotate(rotationSpeed);
+        }
+	}
+}
--- PauseMenu.cs
+++ PauseMenu.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour {
 
 	public static bool SimulationIsPaused = false;
-	
-	public GameObject pauseMenuUI;
-	
-	// Update is called once per frame
-	void Update () {
+    public static string partPath = "Prefabs";
+    public GameObject pauseMenuUI;
+
+    GameObject player;
+    float camX, camY; //used for storing default values
+
+    // Update is called once per frame
+    void Update () {
 		if (Input.GetKeyDown(KeyCode.Escape))
 		{
 			if ( SimulationIsPaused )
@@ -27,7 +31,9 @@ public class PauseMenu : MonoBehaviour {
 	{
 		pauseMenuUI.SetActive(false); 
 		Time.timeScale = 1f;
-		SimulationIsPaused = false;
+        player.GetComponent<PlayerControls>().camSpeedX = camX;
+        player.GetComponent<PlayerControls>().camSpeedY = camY;
+        SimulationIsPaused = false;
 		Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;
 	}
@@ -36,12 +42,34 @@ public class PauseMenu : MonoBehaviour {
 	{
 		pauseMenuUI.SetActive(true);
 		Time.timeScale = 0f;
-		SimulationIsPaused = true;
+        camX = player.GetComponent<PlayerControls>().camSpeedX;
+        camY = player.GetComponent<PlayerControls>().camSpeedY;
+        player.GetComponent<PlayerControls>().camSpeedX = 0;
+        player.GetComponent<PlayerControls>().camSpeedY = 0;
+        SimulationIsPaused = true;
 		Cursor.lockState = CursorLockMode.None;
 		Cursor.visible = true;
 	}
-	
-	public void LoadMenu()
+
+    void Awake()
+    {
+        partPath = System.IO.Path.Combine(Application.dataPath, "Resources/parts.txt");
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void Save()
+    {
+        Debug.Log("Hi");
+        SaveLoadData.Save(partPath, SaveLoadData.partCollection);
+    }
+
+    public void Load()
+    {
+        Debug.Log(partPath);
+        this.transform.GetChild(0).GetChild(2).GetChild(1).gameObject.GetComponent<SaveLoadData>().Load(partPath);
+    }
+
+    public void LoadMenu()
 	{
 		SceneManager.LoadScene("Menu");
 	}
@@ -50,4 +78,32 @@ public class PauseMenu : MonoBehaviour {
 	{
 		Application.Quit();
 	}
+
+    public void SwitchHand()
+    {
+        Transform t = player.transform.GetChild(0).GetChild(0);
+        GameObject text = this.transform.GetChild(0).GetChild(5).GetChild(0).gameObject;
+        bool switched = false;
+        for(int i = 0; i < t.childCount; i++)
+        {
+            if (!switched)
+            {
+                if (t.GetChild(i).gameObject.activeSelf)
+                {
+                    t.GetChild(i).gameObject.SetActive(false);
+                    if (i < t.childCount - 1)
+                    {
+                        t.GetChild(i + 1).gameObject.SetActive(true);
+                        text.GetComponent<Text>().text = "Current tool: " + t.GetChild(i + 1).gameObject.name;
+                    }
+                    else
+                    {
+                        t.GetChild(0).gameObject.SetActive(true);
+                        text.GetComponent<Text>().text = "Current tool: " + t.GetChild(0).gameObject.name;
+                    }
+                    switched = true;
+                }
+            }
+        }
+    }
 }
